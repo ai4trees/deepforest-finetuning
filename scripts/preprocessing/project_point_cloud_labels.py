673034569c6111dd680ef583d4286decb2592f30
Josafat-Mattias Burmeister <josafat-mattias.burm@hpi.de>
2025-04-19 15:46:17 +0200

    fix CI issues

--- scripts/preprocessing/project_point_cloud_labels.py
+++ scripts/preprocessing/project_point_cloud_labels.py
@@ -1,8 +1,10 @@
-"""Projects point cloud tree instance segmentation labels to an orthophoto."""
+"""Script to project point cloud tree instance segmentation labels to an orthophoto."""
 
 from deepforest_finetuning.config import PointCloudLabelProjectionConfig
+from deepforest_finetuning.preprocessing import project_point_cloud_labels
 from deepforest_finetuning.utils import load_config
 
 
 if __name__ == "__main__":
     config = load_config(PointCloudLabelProjectionConfig)
+    project_point_cloud_labels(config)
