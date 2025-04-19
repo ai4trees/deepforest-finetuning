9a299863c177d6eb7f17e5d2433745e7c1e0f9e6
Josafat-Mattias Burmeister <josafat-mattias.burm@hpi.de>
2025-04-19 17:33:50 +0200

    feat: label preprocessing

--- src/deepforest_finetuning/preprocessing/__init__.py
+++ src/deepforest_finetuning/preprocessing/__init__.py
@@ -1,5 +1,7 @@
 """Data preprocessing."""
 
+from .filter_labels import *
+from .preprocess_manually_corrected_labels import *
 from .project_point_cloud_labels import *
 from .rescale_images import *
 
